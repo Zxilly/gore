bff74cc8fbeea9f4d32dd2d26f29cce725902951
Zxilly <zxilly@outlook.com>
2026-08-09 17:22:07 +0800

    feat: extract WebAssembly Go metadata

--- modinfo.go
+++ modinfo.go
@@ -5,6 +5,7 @@
 package gore
 
 import (
+	"bytes"
 	"debug/buildinfo"
 	"errors"
 	"fmt"
@@ -26,6 +27,10 @@ type BuildInfo struct {
 }
 
 func (f *GoFile) extractBuildInfo() (*BuildInfo, error) {
+	if wasm, ok := f.fh.(*wasmFile); ok {
+		return extractBuildInfoFromWasm(wasm)
+	}
+
 	info, err := buildinfo.Read(f.fh.getReader())
 	if err != nil {
 		return nil, fmt.Errorf("error when extracting build information: %w", err)
@@ -37,4 +42,37 @@ func (f *GoFile) extractBuildInfo() (*BuildInfo, error) {
 	}
 
 	return result, nil
+}
+
+// These sentinels are used by cmd/go to delimit the module information string.
+var (
+	modInfoStart = []byte{0x30, 0x77, 0xaf, 0x0c, 0x92, 0x74, 0x08, 0x02, 0x41, 0xe1, 0xc1, 0x07, 0xe6, 0xd6, 0x18, 0xe6}
+	modInfoEnd   = []byte{0xf9, 0x32, 0x43, 0x31, 0x86, 0x18, 0x20, 0x72, 0x00, 0x82, 0x42, 0x10, 0x41, 0x16, 0xd8, 0xf2}
+)
+
+func extractBuildInfoFromWasm(wasm *wasmFile) (*BuildInfo, error) {
+	start := bytes.Index(wasm.memory, modInfoStart)
+	if start < 0 {
+		return nil, ErrNoBuildInfo
+	}
+	start += len(modInfoStart)
+
+	end := bytes.Index(wasm.memory[start:], modInfoEnd)
+	if end < 0 {
+		return nil, ErrNoBuildInfo
+	}
+	if end == 0 {
+		return nil, ErrNoBuildInfo
+	}
+
+	info, err := debug.ParseBuildInfo(string(wasm.memory[start : start+end]))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing WebAssembly build information: %w", err)
+	}
+	version, _ := wasm.getVersion()
+	if version != nil {
+		info.GoVersion = version.Name
+	}
+
+	return &BuildInfo{Compiler: version, ModInfo: info}, nil
 }
